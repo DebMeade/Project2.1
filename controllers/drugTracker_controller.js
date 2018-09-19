3be63822960db7bd9b4d4325e6a374eb1a8a2c71
Jonas Matlock <jpmatlock1017@gmail.com>
2018-09-19 09:38:31 -0600

    added orm for contacts db

--- controllers/drugTracker_controller.js
+++ controllers/drugTracker_controller.js
@@ -1,25 +1,5 @@
-var express = require("express");
-var router = express.Router();
 
-var db = require("../models/drug.js");
 
-module.exports = function(app) {
- 
-
-  // Create a new example
-  router.post("/", function(req, res) {
-    db.create(req.body).then(function(drug_db) {
-      res.json(drug_db);
-    });
-  });
-}
-
-router.get("/drugList", function(req, res) {
-  res.render("index");
-  
-})
-
-module.exports = router;
 
 var express = require("express");
 var router = express.Router();
@@ -28,15 +8,23 @@ var db = require("../models/drug.js");
 
 
 router.get("/", function(req, res){
-  db.all(function(data){
-    var hbsObject = {
+  var hbsObjectOne;
+  db.drugs.all(function(data){
+    hbsObjectOne = {
       drugs: data
     };
-    console.log(hbsObject);
-    res.render("index", hbsObject);
+    console.log(hbsObjectOne);
   });
-});
+    db.contacts.all(function(data){
+      var hbsObjectTwo = {
+        contacts: data
+      };
+      console.log(hbsObjectTwo);
 
+    });
+    res.render("index", hbsObjectOne);
+ 
+});
 
 // module.exports = function(app) {
  
@@ -49,8 +37,6 @@ router.get("/", function(req, res){
 //   });
 // }
 
-// router.get("/drugList", function(req, res) {
-//   res.render("index");
   
 // })
 
--- models/drug.js
+++ models/drug.js
@@ -1,13 +1,35 @@
 var orm = require("../config/orm.js");
 
-var drug = {
+var drugs = {
 
     all: function(cb){
         orm.all("drugs", function(res){
+            cb(res);
+        });
+    },
+
+    //     all: function(cb){
+    //     orm.all("contacts", function(res){
+    //         cb(res);
+    //     });
+    // }
+
+}
+
+var contacts = {
+
+    all: function(cb){
+        orm.all("contacts", function(res){
             cb(res);
         });
     }
 
 }
 
-module.exports = drug;
+//module.exports = drug;
+//module.exports = drugContacts;
+
+module.exports = {
+    drugs: drugs, 
+    contacts: contacts
+}
