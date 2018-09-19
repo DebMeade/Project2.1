c110df0bbcf8d03441764e4d510277a88d9aec8a
Jonas Matlock <jpmatlock1017@gmail.com>
2018-09-19 08:36:26 -0600

    new branch

--- controllers/drugTracker_controller.js
+++ controllers/drugTracker_controller.js
@@ -20,3 +20,39 @@ router.get("/drugList", function(req, res) {
 })
 
 module.exports = router;
+
+var express = require("express");
+var router = express.Router();
+
+var db = require("../models/drug.js");
+
+
+router.get("/", function(req, res){
+  db.all(function(data){
+    var hbsObject = {
+      drugs: data
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
+
+// module.exports = function(app) {
+ 
+
+//   // Create a new example
+//   router.post("/", function(req, res) {
+//     db.create(req.body).then(function(drug_db) {
+//       res.json(drug_db);
+//     });
+//   });
+// }
+
+// router.get("/drugList", function(req, res) {
+//   res.render("index");
+  
+// })
+
+module.exports = router;
+
